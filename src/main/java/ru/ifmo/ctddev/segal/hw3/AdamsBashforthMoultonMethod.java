47450593b9508b08fc0b9e7b4f1204cb6018933c
alex-700 <alex_700_95@mail.ru>
2016-09-15 23:34:11 +0300

    add double corrector

--- src/main/java/ru/ifmo/ctddev/segal/hw3/AdamsBashforthMoultonMethod.java
+++ src/main/java/ru/ifmo/ctddev/segal/hw3/AdamsBashforthMoultonMethod.java
@@ -25,17 +25,19 @@ public class AdamsBashforthMoultonMethod extends MethodForLorenzSystem {
         double curX = x0;
         double curY = y0;
         double curZ = z0;
-        double nextX, nextY, nextZ;
+        double nextX = 0, nextY = 0, nextZ = 0;
         for (double curT = ta; curT <= tb; curT += dt, curX = nextX, curY = nextY, curZ = nextZ) {
             resultBuilder.append(curX, curY, curZ, curT);
             // predictor (Euler method)
             double midX = curX + firstEquation(curX, curY, curZ) * dt;
             double midY = curY + secondEquation(curX, curY, curZ) * dt;
             double midZ = curZ + thirdEquation(curX, curY, curZ) * dt;
-            // corrector (Trapezoid method)
-            nextX = curX + HALF * dt * (firstEquation(curX, curY, curZ) + firstEquation(midX, midY, midZ));
-            nextY = curY + HALF * dt * (secondEquation(curX, curY, curZ) + secondEquation(midX, midY, midZ));
-            nextZ = curZ + HALF * dt * (thirdEquation(curX, curY, curZ) + thirdEquation(midX, midY, midZ));
+            for (int it = 0; it < 2; it++, midX = nextX, midY = nextY, midZ = nextZ) {
+                // corrector (Trapezoid method)
+                nextX = curX + HALF * dt * (firstEquation(curX, curY, curZ) + firstEquation(midX, midY, midZ));
+                nextY = curY + HALF * dt * (secondEquation(curX, curY, curZ) + secondEquation(midX, midY, midZ));
+                nextZ = curZ + HALF * dt * (thirdEquation(curX, curY, curZ) + thirdEquation(midX, midY, midZ));
+            }
         }
         return resultBuilder.toResult();
     }
